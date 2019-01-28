04681171dfd3229a5c31b66927ada524487a7a7b
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2019-01-28 20:05:16 +0530

    fixed build issue

--- src/problems/Linkedlist/isEqual/isEqual.ts
+++ src/problems/Linkedlist/isEqual/isEqual.ts
@@ -1,3 +1,4 @@
+import LinkedList from "../Linkedlist";
 
 export default function IsEqual<T>(listOne: LinkedList<T>, listTwo: LinkedList<T>) {
   return  Object.is(listOne.toString(), listTwo.toString());
